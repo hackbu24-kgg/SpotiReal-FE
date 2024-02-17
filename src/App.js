c9916547127458ac0ea20aee29fd5156b76098bd
AshfaqKhan <gandora86@gmail.com>
2024-02-17 16:37:21 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+
+//const axios = require('axios')
+
+// If there are two parameters: id and category
+const id = 123;
+const category = 'electronics';
+
+// You can add parameters directly to the URL
+axios.get(`http://127.0.0.1:8080/login`)
+  .then(response => {
+    //console.log(getClientID());
+//console.log(getClientSecret());
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
 function App() {
+const appStyle = {
+    border: '2px solid red', 
+    padding: '20px',
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          titles 
+.
+.
+.
+.
+
         </p>
+<p>
+ .
+.
+.
+</p>
+<p>
+.
+.
+.
+</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+	//console.log(getClientID())
+	//console.log(getClientSecret())
         >
           Learn React
         </a>
