73274b855b4846ae653e6adfcb69abf3d87df35e
syoung64 <syoung5@binghamton.edu>
2024-02-17 19:42:23 -0500

    more work on feed component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Feed from './component-Feed/Feed.js';
-import axios from 'axios'
+//import axios from 'axios'
 
-
+// NOTE FROM STELLA: I commented this out because it was breaking the site.
 //const axios = require('axios')
 
 // If there are two parameters: id and category
@@ -11,14 +11,14 @@ const id = 123;
 const category = 'electronics';
 
 // You can add parameters directly to the URL
-axios.get(`http://127.0.0.1:8080/login`)
-  .then(response => {
+//axios.get(`http://127.0.0.1:8080/login`)
+//  .then(response => {
     //console.log(getClientID());
 //console.log(getClientSecret());
-  })
-  .catch(error => {
-    console.error(error);
-  });
+//  })
+//  .catch(error => {
+//    console.error(error);
+//  });
 
 function App() {
 const appStyle = {
@@ -31,26 +31,9 @@ const appStyle = {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          titles <br></br>
           This is working! :D<br></br>
           Save App.js to reload.
-.
-.
-.
-.
-
         </p>
-<p>
- .
-.
-.
-</p>
-<p>
-.
-.
-.
-</p>
-
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/component-Feed/Feed.js
+++ src/component-Feed/Feed.js
@@ -3,7 +3,7 @@ import './Feed.css'
 export default function Feed() {
     return(
         <div>
-            <h1>SpotiReal Feed</h1>
+            <h1>Feed</h1>
             <Post /><br></br>
             <Post /><br></br>
             <Post /><br></br>
@@ -14,8 +14,14 @@ export default function Feed() {
 function Post() {
     return (
         <div className="post">
-            <p>username</p>
-            <img className="albumcover" src="https://media.licdn.com/dms/image/C5612AQHajKUAvBMIjg/article-cover_image-shrink_600_2000/0/1585067392980?e=2147483647&v=beta&t=qMIpxX_oq0TIhrPc9KgNlW2jTtla1FH4nFJFSx_nkh8" alt="placeholder image" />
+            <dt className="username">username</dt>
+            <dd>
+                artist - song name
+                <img className="albumcover" src="https://media.licdn.com/dms/image/C5612AQHajKUAvBMIjg/article-cover_image-shrink_600_2000/0/1585067392980?e=2147483647&v=beta&t=qMIpxX_oq0TIhrPc9KgNlW2jTtla1FH4nFJFSx_nkh8" alt="album cover" />
+                <br></br>
+                timestamp
+                    
+            </dd>
         </div>
     )
 }
