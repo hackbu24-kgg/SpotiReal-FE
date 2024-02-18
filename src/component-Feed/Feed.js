6576f60a4371c6faded7a572fe355df307eb11a4
syoung64 <syoung5@binghamton.edu>
2024-02-17 21:16:06 -0500

    idk

--- src/component-Feed/Feed.js
+++ src/component-Feed/Feed.js
@@ -2,22 +2,33 @@ import './Feed.css'
 
 export default function Feed() {
     return(
-        <div>
+        <div className="feed">
             <h1>Feed</h1>
-            <Post /><br></br>
-            <Post /><br></br>
-            <Post /><br></br>
+            <div><PostButton loggedin={true} /></div>
+            <PostList />
         </div>
     )
 }
 
-function Post() {
+function PostList() {
+    return (
+        <>
+            <Post artist="artist1"/><br></br>
+            <Post artist="artist2"/><br></br>
+            <Post artist="artist3"/><br></br>
+        </>
+    )
+    // need to return a list of however many posts there are
+}
+
+function Post(artist) {
     return (
         <div className="post">
-            <dt className="username">username</dt>
+            <img className="albumcover" src="https://media.licdn.com/dms/image/C5612AQHajKUAvBMIjg/article-cover_image-shrink_600_2000/0/1585067392980?e=2147483647&v=beta&t=qMIpxX_oq0TIhrPc9KgNlW2jTtla1FH4nFJFSx_nkh8" alt="album cover" />
+            <dt className="username">PlaceholderUsername</dt>
             <dd>
                 artist - song name
-                <img className="albumcover" src="https://media.licdn.com/dms/image/C5612AQHajKUAvBMIjg/article-cover_image-shrink_600_2000/0/1585067392980?e=2147483647&v=beta&t=qMIpxX_oq0TIhrPc9KgNlW2jTtla1FH4nFJFSx_nkh8" alt="album cover" />
+                
                 <br></br>
                 timestamp
                     
@@ -26,10 +37,17 @@ function Post() {
     )
 }
 
-function PostButton() {
-    return (
-        <div>
-
-        </div>
-    )
+function PostButton(loggedin) {
+    if (loggedin) {
+        return (
+            <>
+                +
+            </>
+        )
+    }
+    else {
+        return (
+            <></>
+        )
+    }
 }
